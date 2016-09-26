0f8327f7f406553b9920ecbddf2cf67ae4d41710
Yunatan <unatan77@gmail.com>
2016-09-26 15:32:26 +0300

    moving platform

---
+++ src/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MovingPlatform : MonoBehaviour
+{
+    private List<Transform> onPlatform = new List<Transform>();
+
+    private Vector3 lastPos;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        var body = other.rigidbody;
+        if (body == null) return;
+        onPlatform.Add(other.transform);
+        other.transform.parent = transform;
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        onPlatform.Remove(other.transform);
+        other.transform.parent = null;
+    }
+
+    void Start()
+    {
+        lastPos = transform.position;
+    }
+}
